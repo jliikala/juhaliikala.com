092b034e89967f3b8ee94fcf62eb8ade93e081f4
Juha Liikala <jliikala@synopsys.com>
2021-05-22 17:45:00 +0300

    Kirby files

---
+++ site/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+# Kirby Configurations
+return [
+
+    # Kirby Settings
+    'debug' => false,  # Enable Debug Mode
+    'home'  => 'home', # Change the slug for the homepage
+
+    # Markdown Field Configurations
+    'community.markdown-field.font' => [
+        'family'  => 'sans-serif',
+        'scaling' => true,
+        'size'    => 'small',
+    ],
+    'community.markdown-field.modals'     => true,
+    'community.markdown-field.blank'      => false,
+    'community.markdown-field.invisibles' => false
+];
---
+++ site/snippets/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<?= snippet('header/meta')  ?>
+<?= snippet('header/icons') ?>
+    
+<?= css('assets/css/main.min.css') ?>
+
+</head>
+
+<body class="<?= "page-{$page->intendedTemplate()}" ?>">
+    <main class="main">
